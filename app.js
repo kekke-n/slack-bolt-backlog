326c7637f33aaa632ad8aab03cbb7724566fe2f8
Keisuke Nakama <keisuke.nakama@eys-style.com>
2020-09-08 23:37:16 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const { App } = require('@slack/bolt');
+
+// Initializes your app with your bot token and signing secret
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3020);
+
+  console.log('⚡️ Bolt app is running!');
+})();
