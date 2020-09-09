7937cd1b5f7376c4dcbe58bbea34e76634b9e95b
Keisuke Nakama <keisuke.nakama@eys-style.com>
2020-09-09 12:15:40 +0900

    add report bug event handler

--- app.js
+++ app.js
@@ -45,6 +45,61 @@ app.action('button_click', async ({ body, ack, say }) => {
   await say(`<@${body.user.id}> clicked the button`);
 });
 
+app.action({ callback_id: 'report_bug'}, async({action, ack}) => {
+  await ack();
+  await say('登録しました');
+});
+
+
+// open_modal というグローバルショートカットはシンプルなモーダルを開く
+app.shortcut('report_bug', async ({ shortcut, ack, context }) => {
+  // グローバルショートカットリクエストの確認
+  ack();
+
+  try {
+    // 組み込みの WebClient を使って views.open API メソッドを呼び出す
+    const result = await app.client.views.open({
+      // `context` オブジェクトに保持されたトークンを使用
+      token: context.botToken,
+      trigger_id: shortcut.trigger_id,
+      view: {
+        "type": "modal",
+        "title": {
+          "type": "plain_text",
+          "text": "My App"
+        },
+        "close": {
+          "type": "plain_text",
+          "text": "Close"
+        },
+        "blocks": [
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "About the simplest modal you could conceive of :smile:\n\nMaybe <https://api.slack.com/reference/block-kit/interactive-components|*make the modal interactive*> or <https://api.slack.com/surfaces/modals/using#modifying|*learn more advanced modal use cases*>."
+            }
+          },
+          {
+            "type": "context",
+            "elements": [
+              {
+                "type": "mrkdwn",
+                "text": "Psssst this modal was designed using <https://api.slack.com/tools/block-kit-builder|*Block Kit Builder*>"
+              }
+            ]
+          }
+        ]
+      }
+    });
+
+    console.log(result);
+  }
+  catch (error) {
+    console.error(error);
+  }
+});
+
 (async () => {
   // Start your app
   await app.start(process.env.PORT || 3020);
