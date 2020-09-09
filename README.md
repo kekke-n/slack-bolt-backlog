5f003f6278a120d0a50e8150cc2cd8348543dc69
Keisuke Nakama <keisuke.nakama@eys-style.com>
2020-09-09 11:35:04 +0900

    implement tutorial

---
+++ README.md
@@ -0,0 +1,3 @@
+# Reference
+
+https://slack.dev/bolt-js/ja-jp/tutorial/getting-started
--- app.js
+++ app.js
@@ -7,6 +7,44 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
+
+// Listens to incoming messages that contain "hello"
+// app.message('hello', async ({ message, say }) => {
+//   // say() sends a message to the channel where the event was triggered
+//   await say(`Hey there <@${message.user}>!`);
+// });
+
+// Listens to incoming messages that contain "hello"
+app.message('hello', async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  await say({
+    blocks: [
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `Hey there <@${message.user}>!`
+        },
+        "accessory": {
+          "type": "button",
+          "text": {
+            "type": "plain_text",
+            "text": "Click Me"
+          },
+          "action_id": "button_click"
+        }
+      }
+    ],
+    text: `Hey there <@${message.user}>!`
+  });
+});
+
+app.action('button_click', async ({ body, ack, say }) => {
+  // Acknowledge the action
+  await ack();
+  await say(`<@${body.user.id}> clicked the button`);
+});
+
 (async () => {
   // Start your app
   await app.start(process.env.PORT || 3020);
